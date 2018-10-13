0d957ce585c772b92423e9bff9856d371cd4de8f
Suhas <suhasbarghavv@gmail.com>
2018-10-12 23:54:04 -0700

    Initial release.

---
+++ pythonProjects/3__oddOrEven.py
@@ -0,0 +1,10 @@
+##ODD or EVEN
+
+import sys
+
+num = input("Enter a number to determine if it is even or odd:")
+
+if(int(num)%2):
+ print ("The entered number is odd.")
+else:
+ print ("The entered number is even.")
