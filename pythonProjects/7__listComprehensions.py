da291417da0ea665791309bbc898f3643d4ced0a
Suhas <suhasbarghavv@gmail.com>
2018-11-03 21:06:11 -0700

    Added print statement for the new list.

--- pythonProjects/7__listComprehensions.py
+++ pythonProjects/7__listComprehensions.py
@@ -4,6 +4,9 @@ import sys
 list_to_be_processed = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 final_list = []
 
-foreach element in list_to_be_processed:
+for element in list_to_be_processed:
 	if(element%2 == 0):
 		final_list.append(element)
+
+for element in final_list:
+	print(element)
