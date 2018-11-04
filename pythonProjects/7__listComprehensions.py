e0eb7c6906f672f00e29bcf560abfb6a0f59537c
Suhas <suhasbarghavv@gmail.com>
2018-11-03 20:50:49 -0700

    Initial Release.

---
+++ pythonProjects/7__listComprehensions.py
@@ -0,0 +1,9 @@
+##List comprehesions
+import sys
+
+list_to_be_processed = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+final_list = []
+
+foreach element in list_to_be_processed:
+	if(element%2 == 0):
+		final_list.append(element)
